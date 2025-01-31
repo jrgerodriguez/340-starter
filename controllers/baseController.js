de8b9ac4c17276145f5a9edc01784a6f4f7821a3
jrgerodriguez <menjivar.jorge.93@gmail.com>
2025-01-31 08:13:08 -0600

    week4 learning activity complete

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,10 @@
-const utilities = require("../utilities/")
-const baseController = {}
+const utilities = require("../utilities/");
+const baseController = {};
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav() //This will contain the string of HTML to generate the navigation bar
-  res.render("index", {title: "Home", nav}) //What is stored in nav will be sent to index. 
-}
+baseController.buildHome = async function (req, res) {
+  const nav = await utilities.getNav(); //This will contain the string of HTML to generate the navigation bar
+  // req.flash("notice", "This is a flash message.");
+  res.render("index", { title: "Home", nav }); //What is stored in nav will be sent to index.
+};
 
-module.exports = baseController //The object and therefore the buildHome method are exported to be used elsewhere. 
+module.exports = baseController; //The object and therefore the buildHome method are exported to be used elsewhere.
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session");
+const pool = require("./database/");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
@@ -12,7 +14,36 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const utilities = require("./utilities/");
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// bodyParser
+/** In order to collect the values from the incoming request body (which comes from the form), we need to make the application aware of that functionality */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+/** This last line tells the express application to read and work with data sent via a URL as well as from a form, stored in the request object's body. The "extended: true" object is a configuration that allows rich objects and arrays to be parsed. */
 
 /* ***********************
  * View Engine and Templates
@@ -27,13 +58,17 @@ app.set("layout", "./layouts/layout"); // not at views root
 app.use(static);
 
 //Index route
+app.use("/account", accountRoute); //When there is an /account route, it will go through this route
 app.use("/inv", inventoryRoute);
 app.use("/error-link", utilities.handleErrors());
 app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // File Not Found Route - must be last route in list, everytime there is an error, "next" will be used and the error hhandler will be triggered
 app.use(async (req, res, next) => {
-  next({ status: 404, message: "The page you were looking for was not found." });
+  next({
+    status: 404,
+    message: "The page you were looking for was not found.",
+  });
 });
 
 /* ***********************
@@ -44,10 +79,10 @@ app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav();
   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
   let message;
-  if(err.status===404) {
-    message = err.message
+  if (err.status === 404) {
+    message = err.message;
   } else {
-    message = 'Oh no! There was a crash. Maybe try a different route?'
+    message = "Oh no! There was a crash. Maybe try a different route?";
   }
   res.render("errors/error", {
     title: err.status || "Server Error",
